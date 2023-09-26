b2b972f1045470b3cd49a89c60aabe26a73e29c9
Neil Murphy <barrymun@tcd.ie>
2023-09-26 21:58:48 +0100

    client counter component

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,7 @@
-import 'assets/style.css';
+import van from "vanjs-core";
+
+import { Counter } from "components/counter";
+
+import "assets/style.css";
+
+van.add(document.getElementById("app") as HTMLDivElement, Counter());
