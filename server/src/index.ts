e0d92c23f873a48bb0290a14bf5094a4a0eb80f1
Neil Murphy <barrymun@tcd.ie>
2023-09-26 22:08:33 +0100

    [wip] handle ssr from the server package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
+import van from "mini-van-plate/van-plate";
+
+const { body, h1 } = van.tags;
+
 const server = Bun.serve({
   port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
+  fetch(req: Request) {
+    return new Response(
+      van.html(
+        body(
+          h1("SSR with Mini Van Plate"),
+          // Counter({van, value: CountQueuingStrategy, source: "server"}),
+        ),
+      ),
+      { headers: { "Content-Type": "text/html" } },
+    );
   },
 });
 
