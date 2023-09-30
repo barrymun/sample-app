827425560e5dcef7e985381d4d53f6b32cf2bf38
Neil Murphy <barrymun@tcd.ie>
2023-09-30 14:24:13 +0100

    refactor _app and create types

---
+++ client/src/app/utils/types.ts
@@ -0,0 +1,11 @@
+import { type ReactElement, type ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+
+export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,9 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { type ReactElement, type ReactNode } from "react";
-import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
 import "assets/globals.css";
 
-export type NextPageWithLayout<P = Record<string, never>, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
