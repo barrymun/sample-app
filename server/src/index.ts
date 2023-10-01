eabda2afbc61cd5304cdd297f0fede2bd46e58e8
Neil Murphy <barrymun@tcd.ie>
2023-10-01 16:34:37 +0100

    working express server with auth0 authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,37 @@
-const server = Bun.serve({
-  port: 3001,
-  fetch(_req) {
-    return new Response("Bun!");
-  },
+import express from "express";
+const app = express();
+import { auth, requiredScopes } from "express-oauth2-jwt-bearer";
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: "https://dev-uyysrdqjsakrpapo.eu.auth0.com/api/v2/",
+  issuerBaseURL: `https://dev-uyysrdqjsakrpapo.eu.auth0.com/`,
 });
 
-console.log(`Listening on http://localhost:${server.port} ...`);
+// This route doesn't need authentication
+app.get("/api/public", function (req, res) {
+  res.json({
+    message: "Hello from a public endpoint! You don't need to be authenticated to see this.",
+  });
+});
+
+// This route needs authentication
+app.get("/api/private", checkJwt, function (req, res) {
+  res.json({
+    message: "Hello from a private endpoint! You need to be authenticated to see this.",
+  });
+});
+
+const checkScopes = requiredScopes("read:messages");
+
+app.get("/api/private-scoped", checkJwt, checkScopes, function (req, res) {
+  res.json({
+    message:
+      "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.",
+  });
+});
+
+app.listen(3001, function () {
+  console.log("Listening on http://localhost:3001");
+});
