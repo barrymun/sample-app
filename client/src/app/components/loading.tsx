5db044f6494c937f665a08bc7b26628df07ce8c0
Neil Murphy <barrymun@tcd.ie>
2023-10-08 12:58:38 +0100

    loading nextjs image with priority

--- client/src/app/components/loading.tsx
+++ client/src/app/components/loading.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 import loading from "assets/loading.svg";
 
-const Loading = () => <Image src={loading.src} width={loading.width} height={loading.height} alt="Loading" />;
+const Loading = () => <Image src={loading.src} width={loading.width} height={loading.height} alt="Loading" priority />;
 
 export { Loading };
