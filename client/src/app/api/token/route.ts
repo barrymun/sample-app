b083c3c87d392e003616962482d57d5439feb9b7
Neil Murphy <barrymun@tcd.ie>
2023-10-10 13:36:06 +0100

    client Dockerfile, cleanup files with unused modules

--- client/src/app/api/token/route.ts
+++
@@ -1,7 +0,0 @@
-import { getSession } from "@auth0/nextjs-auth0";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const session = await getSession();
-  return NextResponse.json({ token: session?.idToken });
-}
